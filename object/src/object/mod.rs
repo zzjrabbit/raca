20067640df129489d748c2973ccffc345f804552
zzjrabbit <239873059@qq.com>
2026-01-30 19:22:31 +0800

    add two tests

--- object/src/object/mod.rs
+++ object/src/object/mod.rs
@@ -27,23 +27,20 @@ mod tests {
 
     use super::*;
 
+    struct TestObject {
+        field: u32,
+    }
+    impl KernelObject for TestObject {}
+
     #[test]
     fn create_object() {
-        struct TestObject {}
-        impl KernelObject for TestObject {}
-
-        let obj = KObject::new(TestObject {});
+        let obj = KObject::new(TestObject { field: 0 });
         assert!(obj.downcast_ref::<TestObject>().is_some());
         assert!(obj.type_name().ends_with("TestObject"));
     }
 
     #[test]
     fn typed_kobject() {
-        struct TestObject {
-            field: u32,
-        }
-        impl KernelObject for TestObject {}
-
         let obj = TypedKObject::new(TestObject { field: 42 });
         assert!(obj.type_name().ends_with("TestObject"));
         assert_eq!(obj.field, 42);
@@ -51,11 +48,6 @@ mod tests {
 
     #[test]
     fn name() {
-        struct TestObject {
-            field: u32,
-        }
-        impl KernelObject for TestObject {}
-
         let obj = TypedKObject::new(TestObject { field: 42 });
         assert!(obj.type_name().ends_with("TestObject"));
         assert_eq!(obj.field, 42);
@@ -63,15 +55,38 @@ mod tests {
 
     #[test]
     fn as_kobject() {
-        struct TestObject {
-            field: u32,
-        }
-        impl KernelObject for TestObject {}
-
         let obj = TypedKObject::new(TestObject { field: 42 });
         let kobj = obj.as_kobject();
         assert!(kobj.downcast_ref::<TestObject>().is_some());
         assert!(kobj.type_name().ends_with("TestObject"));
         assert_eq!(kobj.downcast_ref::<TestObject>().unwrap().field, 42);
+    }
+
+    #[test]
+    fn weak_typed_kobj() {
+        let obj = TypedKObject::new(TestObject { field: 42 });
+        let weak = obj.downgrade();
+        assert!(weak.upgrade().is_some());
+    }
+
+    #[test]
+    fn weak_kobj() {
+        let obj = KObject::new(TestObject { field: 42 });
+        let weak = obj.downgrade();
+        assert!(weak.upgrade().is_some());
+        assert!(
+            weak.upgrade()
+                .unwrap()
+                .downcast_ref::<TestObject>()
+                .is_some()
+        );
+        assert!(
+            weak.upgrade()
+                .unwrap()
+                .downcast_ref::<TestObject>()
+                .unwrap()
+                .field
+                == 42
+        );
     }
 }
