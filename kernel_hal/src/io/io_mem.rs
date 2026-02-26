6b957f1ece69e298cc7e94b901725f40f5d94e5d
zzjrabbit <239873059@qq.com>
2026-02-26 14:52:22 +0800

    fix IoMem rw

--- kernel_hal/src/io/io_mem.rs
+++ kernel_hal/src/io/io_mem.rs
@@ -46,46 +46,57 @@ impl IoMem {
 }
 
 impl IoMem {
-    pub fn read_bytes(&self, offset: usize, buffer: &mut [u8]) -> Result<(), Error> {
-        let address = self.start_address() + offset;
-        match self
-            .vm_space
-            .reader(address, buffer.len())
-            .read_bytes(buffer)
-        {
+    fn try_map(&self) -> Result<(), Error> {
+        let aligned_addr = align_down_by_page_size(self.start_address());
+        unsafe { VmSpace::kernel() }
+            .cursor(aligned_addr)?
+            .map(
+                &PhysicalMemory::from_start_address(
+                    virt_to_phys(aligned_addr),
+                    self.size().div_ceil(PAGE_SIZE),
+                ),
+                PageProperty::kernel_data(),
+            )
+            .unwrap();
+        Ok(())
+    }
+
+    fn do_with_map(&self, mut f: impl FnMut() -> Result<(), Error>) -> Result<(), Error> {
+        match f() {
             Ok(()) => Ok(()),
             Err(err) if err.errno() == Errno::PageFault => {
-                let aligned_addr = align_down_by_page_size(self.start_address());
-                unsafe { VmSpace::kernel() }
-                    .cursor(aligned_addr)?
-                    .map(
-                        &PhysicalMemory::from_start_address(
-                            virt_to_phys(aligned_addr),
-                            self.size().div_ceil(PAGE_SIZE),
-                        ),
-                        PageProperty::kernel_data(),
-                    )
-                    .unwrap();
-                self.read_bytes(offset, buffer)
+                self.try_map()?;
+                f()
             }
             Err(err) => Err(err),
         }
     }
 
+    pub fn read_bytes(&self, offset: usize, buffer: &mut [u8]) -> Result<(), Error> {
+        let address = self.start_address() + offset;
+        self.do_with_map(|| {
+            self.vm_space
+                .reader(address, buffer.len())
+                .read_bytes(buffer)
+        })
+    }
+
     pub fn read<T: Pod>(&self, offset: usize, value: &mut T) -> Result<(), Error> {
         let address = self.start_address() + offset;
-        self.vm_space.reader(address, size_of::<T>()).read(value)
+        self.do_with_map(|| self.vm_space.reader(address, size_of::<T>()).read(value))
     }
 
     pub fn write_bytes(&self, offset: usize, buffer: &[u8]) -> Result<(), Error> {
         let address = self.start_address() + offset;
-        self.vm_space
-            .writer(address, buffer.len())
-            .write_bytes(buffer)
+        self.do_with_map(|| {
+            self.vm_space
+                .writer(address, buffer.len())
+                .write_bytes(buffer)
+        })
     }
 
     pub fn write<T: Pod>(&self, offset: usize, value: &T) -> Result<(), Error> {
         let address = self.start_address() + offset;
-        self.vm_space.writer(address, size_of::<T>()).write(value)
+        self.do_with_map(|| self.vm_space.writer(address, size_of::<T>()).write(value))
     }
 }
